194b9c8a723cdd148abb014790ba6b88e93e42d1
lgabrielxavier20-afk <l.gabriel.xavier20@gmail.com>
2025-09-26 10:05:39 -0300

    Update PerfilController.java

--- streaming/src/main/java/br/com/plataforma/streaming/controllers/PerfilController.java
+++ streaming/src/main/java/br/com/plataforma/streaming/controllers/PerfilController.java
@@ -22,4 +22,15 @@ public class PerfilController {
     public Perfil salvarPerfil(@RequestBody Perfil perfil) {
         return perfilService.salvarPerfil(perfil);
     }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPerfil(@PathVariable Long id) {
+        perfilService.deletarPerfil(id);
+        return ResponseEntity.noContent().build(); 
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Perfil> atualizarPerfil(@PathVariable Long id, @RequestBody Perfil perfil) {
+        Perfil atualizado = perfilService.atualizarPerfil(id, perfil);
+        return ResponseEntity.ok(atualizado);
+    }
 }
